47525faae07785e1b37572fa3ed4b4eb5edacabd
ZhangSherlock <33927720+ZhangSherlock@users.noreply.github.com>
2017-12-01 17:57:43 +0800

    Add files via upload

--- SlefAdaption.cs
+++ SlefAdaption.cs
@@ -1,58 +1,34 @@
-﻿/* 
- *Copyright(C) 2017 by zhanghao 
- *All rights reserved. 
- *FileName:     SlefAdaption 
- *Version:      2017 
- *UnityVersion：5.6.2
- *Date:         2017-12-1
- *Description:    
- *History: 
-*/
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
-using System.Runtime.InteropServices;
-using System;
 
 public class SlefAdaption : MonoBehaviour
 {
+    public Transform _cubeOne;
+    public Transform _cubeTwo;
+
+    public Transform _cubeThree;
+    public Transform _cubeFour;
     private ImageTargetBehaviour mImageTargetBehaviour = null;
-
-    int width;
-
-    int height;
-
-    int leftUpPositionX;
-
-    int leftUpPositionY;
-
-    [DllImport("user32.dll")]       
-
-    public static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int Width, int Height, int flags);
-    //参数分别是：x,y分别表示窗口左上角在屏幕的什么位置，width，height为0表示不改变窗口大小。
-
-    [DllImport("user32.dll")]
-
-    public static extern IntPtr GetActiveWindow();
-    //获得当前窗口
+    Vector3 cubeSceenPoint;
+    Vector3 cubeTwoSceenPoint;
+    Vector3 cubeThreeSceenPoint;
+    Vector3 cubeFourSceenPoint;
     void Start()
     {
-
+        // We retrieve the ImageTargetBehaviour component
+        // Note: This only works if this script is attached to an ImageTarget
         mImageTargetBehaviour = GetComponent<ImageTargetBehaviour>();
 
         if (mImageTargetBehaviour == null)
         {
             Debug.Log("ImageTargetBehaviour not found ");
         }
-
-        StartCoroutine(ForceRes());//此协程改变分辨率及更改程序运行窗口大小
     }
 
- 
     void Update()
     {
-
         if (mImageTargetBehaviour == null)
         {
             Debug.Log("ImageTargetBehaviour not found");
@@ -60,36 +36,64 @@ public class SlefAdaption : MonoBehaviour
         }
 
         Vector2 targetSize = mImageTargetBehaviour.GetSize();//得到imagetarget的像素大小
-
+      
         float targetAspect = targetSize.x / targetSize.y;
 
+        // We define a point in the target local reference 
+        // we take the bottom-left corner of the target, 
+        // just as an example
+        // Note: the target reference plane in Unity is X-Z, 
+        // while Y is the normal direction to the target plane
         Vector3 pointOnTarget = new Vector3(-0.5f, 0, -0.5f / targetAspect);//pointOnTarget=new Vector3(-0.5,0,-0.37)
 
+        Vector3 cubeOneCubeOnTarget = _cubeOne.position;
+        Vector3 cubeTwoCubeOnTarget = _cubeTwo.position;
 
+
+        Vector3 cubeThreeCubeOnTarget = _cubeThree.position;
+        Vector3 cubeFourCubeOnTarget  = _cubeFour.position;
+
+        // We convert the local point to world coordinates
         Vector3 targetPointInWorldRef = transform.TransformPoint(pointOnTarget);//将imagetarget的本地坐标转化成世界坐标
 
+        Vector3 cubePointInWorldRef = transform.TransformPoint(cubeOneCubeOnTarget);
+        Vector3 cubeTwoPointInWorldRef = transform.TransformPoint(cubeTwoCubeOnTarget);
 
+        Vector3 cubeThreePointInWorldRef = transform.TransformPoint(cubeThreeCubeOnTarget);
+        Vector3 cubeFourPointInWorldRef = transform.TransformPoint(cubeFourCubeOnTarget);
+        // We project the world coordinates to screen coords (pixels)
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(targetPointInWorldRef); //将imagetarget的世界坐标转化成屏幕坐标
 
-        Debug.Log("target point in screen coords: " + screenPoint.x + ", " + screenPoint.y);
+        cubeSceenPoint = Camera.main.WorldToScreenPoint(cubePointInWorldRef);
+        cubeTwoSceenPoint = Camera.main.WorldToScreenPoint(cubeTwoPointInWorldRef);
 
-        leftUpPositionX = Math.Abs((int)screenPoint.x);//将imagetarget的x轴屏幕坐标转化成绝对值形式赋给变量leftUpPositionX
+        cubeThreeSceenPoint = Camera.main.WorldToScreenPoint(cubeThreePointInWorldRef);
+        cubeFourSceenPoint = Camera.main.WorldToScreenPoint(cubeFourPointInWorldRef);
 
-        leftUpPositionY = Math.Abs((int)screenPoint.y);//将imagetarget的y轴屏幕坐标转化成绝对值形式赋给变量leftUpPositionY
+
+
+
     }
-
-    IEnumerator ForceRes()//此协程改变程序运行窗口分辨率及更改程序运行窗口左上角位置
+    private void OnGUI()
     {
-        while (true)
+        float AB = (cubeTwoSceenPoint.x - cubeThreeSceenPoint.x) * (cubeTwoSceenPoint.x - cubeSceenPoint.x) + (cubeTwoSceenPoint.y - cubeThreeSceenPoint.y) * (cubeTwoSceenPoint.y - cubeSceenPoint.y);
+        GUI.Label(new Rect(10, 100, 300, 100), "左上顶点坐标：" + "(" + cubeSceenPoint.x + "," + cubeSceenPoint.y + ")");
+        GUI.Label(new Rect(10, 200, 300, 100), "右下顶点坐标：" + "(" + cubeThreeSceenPoint.x + "," + cubeThreeSceenPoint.y + ")");
+        GUI.Label(new Rect(10, 300, 300, 100), "长：" + "(" + (cubeTwoSceenPoint.x- cubeSceenPoint.x) + ",宽：" +(cubeSceenPoint.y- cubeTwoSceenPoint.y) + ")");
+
+        GUI.Label(new Rect(300, 300, 300, 100), AB + "");
+        if (AB==0)
         {
-            yield return new WaitForSeconds(2f);
-
-            Screen.SetResolution(500,500, false);
-
-            yield return new WaitForSeconds(1f);
-
-            SetWindowPos(GetActiveWindow(), 0, leftUpPositionX, leftUpPositionY, 5, 5, 0x001);
+            GUI.Label(new Rect(300, 100, 300, 100), "12与32垂直");
         }
+        else
+        {
+            GUI.Label(new Rect(300, 100, 300, 100), "12与32不垂直");
+        }  
+        
+     
+
+        
     }
 }
 
